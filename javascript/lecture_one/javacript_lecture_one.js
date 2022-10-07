2fcb8bb7257956f64b5497120ad27437df05e1eb
Lukovnikov Vadim <karhashim@icloud.com>
2022-10-07 22:42:49 +0300

    1

---
+++ javascript/lecture_one/javacript_lecture_one.js
@@ -0,0 +1,12 @@
+const nameOurPlanet = "Земля"; // Название нашей планеты
+console.log(nameOurPlanet);
+let currentUserTime = "21:00"; // Текущее время пользователя
+console.log(currentUserTime);
+let numberArticle = 4; // Число статей
+console.log(numberArticle);
+let cashPayment = true; // Оплата наличными
+console.log(cashPayment);
+let firstName = "Иван"; // Первое имя
+let secondName = "Иванович"; // Второе имя
+let thirdName = "Иванов"; // Третье имя
+console.log(firstName, secondName, thirdName);
