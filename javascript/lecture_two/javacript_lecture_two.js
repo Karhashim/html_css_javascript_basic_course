1bc0d316ed9eda84e404d22701a4383567e84ca6
Lukovnikov Vadim <karhashim@icloud.com>
2022-10-07 22:45:18 +0300

    1

---
+++ javascript/lecture_two/javacript_lecture_two.js
@@ -0,0 +1,38 @@
+//console.log("Hello world!");
+
+//const ourPlanetName = "Земля";
+
+//console.log(ourPlanetName);
+
+//const name = "Alex";
+
+//const surName = "Petrov";
+
+//const age = 46;
+
+//const userData = `Имя пользователя: ${name}, возраст: ${age}`;
+
+//const userDataNew = `Имя пользователя: ${name}, возраст: ${age}`;
+
+//console.log(userData);
+
+//const isUser = false;
+
+/*const isVisible = true;
+const isVisible2 = true;*/
+
+//let townName;
+
+//console.log(townName);
+
+//townName = "Moscow";
+
+//console.log(townName);
+
+/*const userData = {
+  name: "Ivan",
+  age: "34",
+};
+
+console.log(userData);*/
+
