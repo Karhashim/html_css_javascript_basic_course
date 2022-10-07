c5018ac1aa9738fe3ed58634826679f34f1ef9f2
Lukovnikov Vadim <karhashim@icloud.com>
2022-10-07 22:45:53 +0300

    1

---
+++ javascript/lecture_three/javacript_lecture_three.js
@@ -0,0 +1,82 @@
+/*const name = "Alex";
+
+const nameToNumber = Number(name);
+
+console.log(nameToNumber);
+
+const age = 26;
+
+const ageToString = String(age);
+
+console.log(typeof(ageToString));
+
+const nullValue = null;
+console.log(Number(nullValue));
+
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean({}));*/
+
+//console.log(typeof(+"45"));
+//console.log(typeof(Number("45")));
+
+/*console.log(2==3);
+console.log(2==2);
+console.log(2!=2);
+console.log(2<3);
+console.log(2>3);
+console.log(2+2+"3");
+console.log("2" +2+3);
+console.log("2"+"3");
+console.log(2+3);*/
+
+/*console.log("3"*2);
+console.log(typeof("3"+"1"));
+console.log(typeof(+"1"));*/
+//console.log(typeof(2 + 2 + "2" + 3 + 3));
+//console.log(typeof(2 + 2 + "2" + 3 * 3));
+//console.log("a      ">="a");
+/*console.log(2==2);
+console.log(2=="2");
+console.log(2==="2");
+console.log(2===2);*/
+/*console.log(5>4);
+console.log("ананас">"яблоко");
+console.log("2">"12");
+console.log(undefined==null);
+console.log(undefined===null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log("4" - 2);
+console.log(7 / 0);
+console.log("  -9  " + 5);
+console.log("  -9  " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2);*/
+
+/*const towns = ["Mocsow", "Paris", "London", "NN"];
+console.log(towns.length);
+towns[1] = "Milan"
+towns[4] = "St. Peterburg"
+
+towns[9] = "Berlin",
+console.log(towns.length);*/
+/*const towns = ["Mocsow", "Paris", "London", "NN"];
+console.log(towns[1]);
+console.log(towns[-1]);*/
+
+const films = ["Back to the Future", "The Terminator", "Jurassic Park"];
+console.log(films[0]);
+films.push("Alien");
+console.log(films[films.length-1]);
