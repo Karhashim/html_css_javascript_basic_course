fbd624da294f4227bb73155266e44f527ba81493
Lukovnikov Vadim <karhashim@icloud.com>
2022-10-25 19:34:10 +0300

    1

---
+++ javascript/lecture_six/javascript_lecture_six.js
@@ -0,0 +1,36 @@
+/* const person = {
+    name: "Alex",
+};
+console.log(person);
+person.age = 56;
+person["city-name"] = "NN"
+console.log(person); */
+/* const myFavoriteFilm = {
+nameFilm: "name", */
+/* releaseDate: 2020,
+  nameDirector: "first name",
+  countryOfIssue: "Country", */
+/* nameFilm: function () {
+    console.log("name");
+  },
+}; */
+// console.log(myFavoriteFilm);
+// myFavoriteFilm.rentalRevenue = 1000;
+// console.log(myFavoriteFilm);
+// delete myFavoriteFilm.releaseDate;
+// console.log(myFavoriteFilm);
+// console.log(myFavoriteFilm.nameFilm);
+/* myFavoriteFilm.nameFilm() */
+// console.log(parseInt("20sss30"));
+// console.log(isNaN(1));
+/* let a = 12.11111;
+console.log(a.toFixed(10)); */
+// console.log("Ivan".length);
+let a = 11111;
+const isUpperCase = (str, i) => {
+  if (str[i] === str[i].toUpperCase()) {
+    return true;
+  } else
+  return false;
+};
+console.log(isUpperCase(a, 2));
