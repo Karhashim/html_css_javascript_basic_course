1b2ed6da6caec18e0a7b29d4fa84a1fc8e2d6c55
Lukovnikov Vadim <karhashim@icloud.com>
2022-11-04 13:46:09 +0300

    1

---
+++ javascript/lecture_five/javascript_lecture_five.js
@@ -0,0 +1,33 @@
+/* const yearOfFly = 1961;
+let count = 0; //счетчик итераций
+for (let i = 2022; yearOfFly < i; i-=1) { //цикл начинается с 2022; пока yearOfFly меньше i, условие правдиво, цикл выполняется; i уменьшается на 1 с каждой итерацией (шагом)
+    count +=1;
+} 
+console.log(count) */
+// const yearOfFly = 1961;
+/* let count = 0;
+for (let i = 2022; 1800 < i; i -= 1) {
+    if (i === 1961) {
+    break; //остановка цикла
+  }
+  count += 1;
+}
+console.log(count); */
+/* function showMessage (message) {
+    console.log(message);
+}
+showMessage("Hi")
+showMessage("Hello")
+showMessage("Привет")
+showMessage("Как дела") */
+/* function sum(a, b) {
+  return a + b
+}
+const result = sum(3, 5)
+console.log(result) */
+let a = 1;
+function mathematicalAddition(b) {
+  let a = 2;
+  return a+b;
+}
+console.log(mathematicalAddition(3));
---
+++ javascript/lecture_seven/javascript_lecture_seven.js
@@ -0,0 +1,11 @@
+/* const names = ["Alex", "Ivan", "Tony", "Olga"]
+const findElem = names.find((a) => a === "Tony");
+console.log(findElem) */
+/* const arr = [1, -2, 5, -6, 9, -8, 5]
+const result = arr.reduce((prev, cur) => {
+    if (cur > 0) {
+        return prev + cur;
+    }
+    return prev
+});
+console.log(result) */
